93ee406b1078737f34be9fa3cb20635dd7d3d7ea
Brazen Kallio <brazen101@hotmail.ca>
2015-01-25 00:40:27 -0500

    Created Hero basic Hero class template.

--- src/Hero.java
+++ src/Hero.java
@@ -1,4 +1,45 @@
 
 public class Hero {
+	private int strength;
+	private int speed;
+	private int health;
+	public String name;
+	
+	//---------------------Constructor---------------------
+	public Hero(String name) {
+		this.name = name;
+		generateAbilities();
+	}
+	
+	//-----------------------Getters-----------------------
+	public int getStrength() {
+		return strength;
+	}
 
+	public int getSpeed() {
+		return speed;
+	}
+
+	public int getHealth() {
+		return health;
+	}
+	
+	//-------------------Private Methods--------------------
+	private void generateAbilities() {
+		
+	}
+	
+
+	public void fight() {
+		
+	}
+	
+	public boolean hitAttempt() {
+		return false;
+	}
+	
+	public int hitDamage() {
+		return health;
+		
+	}
 }
