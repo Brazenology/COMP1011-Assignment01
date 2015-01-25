5dc0a8c5a9e7f46287888857162b7e66aa0e8206
Brazen Kallio <brazen101@hotmail.ca>
2015-01-25 01:35:07 -0500

    Completed main class.

--- src/Project.java
+++ src/Project.java
@@ -1,15 +1,19 @@
 /**
  * @author Brazen
- *
+ *Main Class of COMP1011 - Assignment 1.
+ *Creates a hero, displays his ability statistics, 
+ *determines if he has successfully hit a target and 
+ *how much damage was dealt.
+ *@Modified January 25, 2015.
  */
 public class Project {
 
 	/**
-	 * @param args
+	 * Creates a hero, displays his ability statistics and initiates a fight.
 	 */
 	public static void main(String[] args) {
-		
-
+		Hero hero = new Hero("Nighthawk");
+		hero.show();
+		hero.fight();
 	}
-
 }
